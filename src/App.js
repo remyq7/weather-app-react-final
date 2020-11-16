c7efea09b1529c4c333d9d08e8a7d93ebd041ca6
Vy <remy_q7@yahoo.com>
2020-11-15 19:30:42 -0500

    update temperature for celsius and fahrenheit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import Weather from "./weather.js";
+import Weather from "./Weather.js";
 import Search from "./Search.js";
 
 function App() {
   return (
     <div className="search">
-      <Search/>
-        <h1>New York City </h1>
+      <Search /> 
+        <h1>New York City</h1>
       <div className="card">
-        <h2>Monday, 10:22,</h2>
+        <h2>Monday, 10:22,</h2></div>
+        
+    <div className="Temperature">
+        <a href="#" className="celsius">
+          °C{" "}
+        </a>
+        |{" "}
+        <a href="#" className="fahrenheit">
+          °F
+        </a>
+    
+      </div>
         <img
           src="https://www.publicdomainpictures.net/pictures/310000/velka/orange-circle.png"
           alt="clear"
           className="icon"
-        />
+        /><div>
+        <Weather/>
 
       </div>
       <br />
