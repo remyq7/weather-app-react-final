4333e7a8aaa95e4797ba17ac14902b480e4c837f
Vy <remy_q7@yahoo.com>
2020-11-15 19:08:03 -0500

    Add Search function and css

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import Weather from "./weather.js";
+import '/index.css';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Hello Vera! Great Job! This is awesome!</h1>
-        <Weather city="Manila"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="search">
+      <Search/>
+        <h1>New York City </h1>
+      <div className="card">
+        <h2>Monday, 10:22,</h2>
+        <img
+          src="https://www.publicdomainpictures.net/pictures/310000/velka/orange-circle.png"
+          alt="clear"
+          className="icon"
+        />
+
+        
+      </div>
+      <br />
+     
+      <h5>
+        <a href="https://github.com/remyq7/React" alt="GitHUb page">
+          Open-source code by{" "}
         </a>
-      </header>
+        Vera Malanyaon
+      </h5>
     </div>
   );
+      
+      
+   
 }
 
 export default App;
