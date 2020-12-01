d13740d41d8b244d8c95b21ac5517c9c27430dcc
Vy <remy_q7@yahoo.com>
2020-11-30 23:17:44 -0500

    Update app.js

--- src/App.js
+++ src/App.js
@@ -9,26 +9,12 @@ export default function App() {
     <div className="search">
       <div className="container">
       <Search /> 
-        <h1>New York City</h1>
-      <div className="card">
-        <h2>Monday, 10:22,</h2></div>
-        
-    <div className="Temperature">     
-          <strong>°C{" "} </strong>|{" "} °F
-      </div>
-        <img
-          src="https://www.publicdomainpictures.net/pictures/310000/velka/orange-circle.png"
-          alt="clear"
-          className="icon"
-        /><div>
-      </div>
-      <div className="Forecast">
-        This is the forecast</div>
+      <Forecst/>
       <br />
       <h5>
         <a href="https://github.com/remyq7/weather-app-react-final" alt="GitHUb page">
           Open-source code by{" "}
-        Vera Malanyaon </a>and <a href="https://tender-neumann-2ce604.netlify.app" target="_blank"> hosted on Netlify</a>
+        Vera Malanyaon </a> and <a href="https://tender-neumann-2ce604.netlify.app" target="_blank"> hosted on Netlify</a>
       </h5>
     </div>
     </div>
