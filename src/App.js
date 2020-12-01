875e0fd157f34facba5a44a82efb4b442f7bcc2d
Vy <remy_q7@yahoo.com>
2020-12-01 02:54:48 -0500

    Update App.css, App.js and Search.js

--- src/App.js
+++ src/App.js
@@ -7,34 +7,16 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Search defaultCity="New York"/>
-        <form id="city-form">
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Type City here"
-                className="form-control"
-                autoFocus="on"/>
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
-            </div>
             <h1>New York City</h1>
           </div>
-        </form>
-        
-      <br />
+  
       <h5>
         Open-source code by{" "}
         <a href="https://github.com/remyq7/weather-app-react-final" alt="GitHUb page">
         Vera Malanyaon </a> 
       </h5>
     </div>
-    </div>
+    
   );
 }
 
--- src/Search.js
+++ src/Search.js
@@ -65,6 +65,22 @@ export default function Weather(props) {
     );
   } else {
     search();
-    return (<div>"Loading..."</div>);
+    return (
+    <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type City here"
+                className="form-control"
+                autoFocus="on"/>
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div></div>);
+            
   }
 }
